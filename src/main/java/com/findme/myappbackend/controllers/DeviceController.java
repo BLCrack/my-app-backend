230164d4aad051388b93240455e0b8c72ba17a2d
Krzysztof Graczyk <krzysztof.graczyk@contractors.roche.com>
2018-05-05 14:24:36 +0200

    added cors support

--- src/main/java/com/findme/myappbackend/controllers/DeviceController.java
+++ src/main/java/com/findme/myappbackend/controllers/DeviceController.java
@@ -3,15 +3,15 @@ package com.findme.myappbackend.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.findme.myappbackend.models.Device;
 import com.findme.myappbackend.models.User;
 import com.findme.myappbackend.services.DeviceService;
 
 @RestController
+@RequestMapping("/devices")
+@CrossOrigin
 public class DeviceController {
 	@Autowired
 	DeviceService deviceService;
@@ -20,17 +20,18 @@ public class DeviceController {
 	User user = new User("admin", "admin");
 	//
 
-	@GetMapping(value = "/devices")
+	@GetMapping(value = "/all")
 	public List<Device> showDevices()
 	{
+		System.out.println("show devices");
 		//TESTY
 		user.setId(1);
 		//
 		return deviceService.findAllForUser(user);
 	}
 
-	@PostMapping(value = "/adddevice")
-	public Device addDevice(Device device)
+	@PostMapping(value = "/add")
+	public Device addDevice(@RequestBody Device device)
 	{
 		//1. DODANIE URZADZENIA DO UZYTKOWNIKA
 		//TESTY
